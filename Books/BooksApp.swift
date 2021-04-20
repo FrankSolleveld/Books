8906a19685ec71f5e71e79eb020ed9091416e91a
FrankSolleveld <fsolleveld@outlook.com>
2021-04-20 14:29:43 +0200

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Frank Solleveld on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
